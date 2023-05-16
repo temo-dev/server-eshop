97f7d48adb58bb3ae9be32646cc5bd7fa259a744
TuanAnh-Seta <anhnt6879@seta-international.vn>
2023-05-16 21:00:42 +0200

    check

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,22 +1,13 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable({})
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient {
   constructor() {
     super({
       datasources: {
         db: { url: 'file:./db/dev.db' },
       },
-    });
-  }
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
     });
   }
 }
