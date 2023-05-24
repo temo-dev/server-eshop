7383e9c1cb40991924128f0e0cbd9f724e9b2a8a
TuanAnh-Seta <anhnt6879@seta-international.vn>
2023-05-24 14:01:20 +0200

    fix-db

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-
 @Injectable({})
 export class PrismaService extends PrismaClient {
   constructor() {
     super({
       datasources: {
-        db: { url: 'file:./db/dev.db' },
+        db: {
+          url: 'postgres://temo-dev:S15UHqcolJTB@ep-steep-math-885945-pooler.eu-central-1.aws.neon.tech/neondb?pgbouncer=true&connect_timeout=10',
+        },
       },
     });
   }
